7c2149ee4caa361469ce4fce77c134e6d3930e52
davimattos <davimattosdev@gmail.com>
2019-08-05 21:31:26 -0300

    test a form submit new tech

--- __tests__/components/TechList.test.js
+++ __tests__/components/TechList.test.js
@@ -5,14 +5,19 @@ import TechList from "~/components/TechList";
 
 describe("TechList component", () => {
   it("should be able to add new tech", () => {
-    const { getByText, getByTestId, debug } = render(<TechList />);
+    const { getByText, getByTestId, debug, getByLabelText } = render(
+      <TechList />
+    );
 
-    debug();
+    // debug();
 
-    fireEvent.click(getByText("Adicionar"));
+    fireEvent.change(getByLabelText("Tech"), { target: { value: "Node.js" } });
+    fireEvent.submit(getByTestId("tech-form"));
 
-    debug();
+    // debug();
 
     expect(getByTestId("tech-list")).toContainElement(getByText("Node.js"));
+
+    expect(getByLabelText("Tech")).toHaveValue("");
   });
 });
